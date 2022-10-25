d7c0e089f4b04d4051f6c8dccb7ef98c7b60bb76
Tom Meulenkamp <t.meulenkamp@roboteamtwente.nl>
2022-10-25 10:33:36 +0200

    Slugified the team urls so they look nicer

--- teams/urls.py
+++ teams/urls.py
@@ -3,10 +3,6 @@ from django.urls import path
 from teams import views
 
 urlpatterns = [
- path(r'<int:pk>/', views.TeamDetailView.as_view(), name="team"),
- path('dtt/', views.DroneTeamView.as_view(), name="Drone Team Twente"),
- path('est/', views.ElectricSuperBikeView.as_view(), name="Electric Superbike Twente"),
- path('gtt/', views.GreenTeamView.as_view(), name="Green Team Twente"),
- path('rtt/', views.RoboTeamView.as_view(), name="RoboTeam Twente"),
- path('sbt/', views.SolarBoatView.as_view(), name="Solar Boat Twente"),
+ # path('<int:pk>/', views.TeamDetailView.as_view(), name="team"),
+ path('<slug:slug>/', views.TeamDetailView.as_view(), name="team"),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import TemplateView, DetailView
-from django.views.generic.base import ContextMixin
+from django.views.generic import DetailView
 
 from teams.models import Team
 
@@ -12,30 +11,3 @@ class TeamDetailView(DetailView):
         context = super(TeamDetailView, self).get_context_data(**kwargs)
         context['current'] = "teams"
         return context
-
-class TeamMixin(ContextMixin):
-
-    def get_context_data(self, **kwargs):
-        context = super(TeamMixin, self).get_context_data(**kwargs)
-        context['current'] = "teams"
-        return context
-
-
-class DroneTeamView(TemplateView, TeamMixin):
-    template_name = "drone_team.html"
-
-
-class ElectricSuperBikeView(TemplateView, TeamMixin):
-    template_name = "electric_superbike.html"
-
-
-class GreenTeamView(TemplateView, TeamMixin):
-    template_name = "green_team.html"
-
-
-class RoboTeamView(TemplateView, TeamMixin):
-    template_name = "robo_team.html"
-
-
-class SolarBoatView(TemplateView, TeamMixin):
-    template_name = "solar_boat.html"
