2693ac76a0ccb43af1a6048f8b0d0d58dc62925d
Tom Meulenkamp <meulenkampt@gmail.com>
2022-12-18 19:11:56 +0100

    Update summary to title in admin view

--- events/admin.py
+++ events/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
-# Register your models here.
+
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('summary', 'visible')
+    list_display = ('title', 'visible')
+
 
 admin.site.register(Event, EventAdmin)
