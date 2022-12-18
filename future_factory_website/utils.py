cd55b09c08bb3d91b414f9425b75907a763cb10b
Tom Meulenkamp <meulenkampt@gmail.com>
2022-12-18 18:37:13 +0100

    Auto compress images
    Images that are now uploaded to the FF website are automatically compressed. Allowing for faster page load times

---
+++ future_factory_website/utils.py
@@ -0,0 +1,20 @@
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
+
+
+def compress(image, quality=60):
+    """
+    Compresses a given image and returns it.
+
+    :param image   The image to be compressed
+    :param quality The quality of the resulting image
+    """
+    img = Image.open(image)
+    img_io = BytesIO()
+    if '.png' not in image.name.lower():
+        img.save(img_io, format='JPEG', quality=quality)
+    else:
+        img.save(img_io, format='PNG', quality=quality)
+    new_img = File(img_io, name=image.name)
+    return new_img
