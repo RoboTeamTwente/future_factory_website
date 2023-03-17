b77174a41242cfc1cc1619d7d5fc521f649af9e1
Tom Meulenkamp <meulenkampt@gmail.com>
2023-03-17 13:34:19 +0100

    Send mail using noreply account from RoboTeam Twente

--- main_site/views.py
+++ main_site/views.py
@@ -1,6 +1,8 @@
 import datetime
+from smtplib import SMTPException
 
 from django.contrib import messages
+from django.core.mail import send_mail, BadHeaderError, mail_admins
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views import View
@@ -50,10 +52,21 @@ class SendMessage(View):
         contact_form = ContactForm(request.POST)
 
         if contact_form.is_valid():
-            Email.objects.create(email_sender=contact_form.cleaned_data['sender_mail'],
-                                 recipient=self.team,
-                                 message=contact_form.cleaned_data['message'])
-            messages.success(request, message="Your email was successfully send")
+            message = f"You got a new message from the Future Factory website!\n\nFrom: {contact_form.cleaned_data['sender_mail']}\n\nMessage: {contact_form.cleaned_data['message']}"
+            try:
+                send_mail(subject="[Future Factory Website] General contact form",
+                          from_email='noreply@roboteamtwente.nl',
+                          recipient_list=[self.team.contact_mail if self.team else 'info@futurefactorytwente.nl'],
+                          message=message)
+                messages.success(request, message="Your email was successfully send")
+            except SMTPException as e:
+                messages.error(request, message="The server cannot send your email. Please try again later.")
+                mail_admins(subject="[Contact Form] Failed sending email", message=str(e))
+            except BadHeaderError:
+                messages.error(request, message="Your email seems to be malformed, please try again.")
+            # Email.objects.create(email_sender=contact_form.cleaned_data['sender_mail'],
+            #                      recipient=self.team,
+            #                      message=contact_form.cleaned_data['message'])
         else:
             for field in contact_form.errors:
                 messages.error(request, message=contact_form.errors.get_json_data()[field][0]['message'])
